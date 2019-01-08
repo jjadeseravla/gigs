db0894925106bf5f73b55077aa1ad7db87e0446e
jade <jadealvares@hotmail.co.uk>
2019-01-08 19:58:40 +0000

    look at node modules intializer you had no password and wrong username

--- app.js
+++ app.js
@@ -3,6 +3,34 @@ const exphbs = require('express-handlebars');
 const bodyParser = require('body-parser');
 const path = require('path');
 
+const Sequelize = require('sequelize');
+
+const db = new Sequelize('codegig', 'jadealvares', null, {
+  host: 'localhost',
+  dialect: 'postgres',
+  operatorsAliases: false,
+
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  },
+});
+
+//test db
+// db.authenticate()
+//   .then(() => console.log('Db connected...'))
+//   .catch(err) => console.log('Error:' err))
+db
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
+
 const app = express();
 
 app.get('/', function(req, res) {
