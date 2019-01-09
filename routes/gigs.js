c563cb554784a4672268288c597536cf2d2be0e0
jade <jadealvares@hotmail.co.uk>
2019-01-09 23:22:05 +0000

    hardcoded add route and is now successfully putting that into psql db and i can see it on pgadmin tool

--- routes/gigs.js
+++ routes/gigs.js
@@ -3,12 +3,38 @@ const router = express.Router();
 const db = require('../config/database');
 const Gig = require('../models/Gig');
 
-router.get('/', (req, res) => 
+//get gig list
+router.get('/', (req, res) =>
   Gig.findAll() //method in SQL that returns a promise
     .then(gigs => {
       console.log(gigs);
       res.sendStatus(200);
     })
     .catch(err => console.log(err)));
+
+//add a gig
+router.get('/add', (req, res) => {
+  const data = {
+    title: 'looking for a react developer',
+    technologies: 'react, javascript, html, css',
+    budget: '£4000',
+    description: 'this will be a 3 month contract',
+    contact_email: 'jade@gmail.com'
+  }
+
+  let { title, technologies, budget, description, contact_email } = data;
+
+  //insert into table-hints
+  Gig.create({
+    title: title,
+    technologies: technologies,
+    description: description,
+    budget: budget,
+    contact_email: contact_email
+  })
+    .then(gig => res.redirect('/gigs'))
+    .catch(err => console.log(err));
+});
+
 
 module.exports = router;
